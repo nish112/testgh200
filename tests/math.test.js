abfb54ab7a6496da4f69a2ee74461b1f1ba51c6a
Nishant Srivatava <bigdata.nishant@gmail.com>
2026-08-23 13:08:06 +0530

    Update math.test.js

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,9 +1,9 @@
 const sum = require('../src/math');
 
-test('adds 1 + 2 to equal 3', () => {
+test('adds 1 + 3 to equal 4', () => {
     expect(sum(1, 2)).toBe(3);
 });
 
-test('adds -1 + -1 to equal -2', () => {
+test('adds -1 + -2 to equal -3', () => {
     expect(sum(-1, -1)).toBe(-2);
 });
